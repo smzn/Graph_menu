982b3e7503f6ee1d9ddce58b7272401d1c82eca5
mizuno <mizuno@192.168.1.10>
2018-01-15 05:28:29 +0900

    グラフ表示(メニュー、カードレイアウト利用)

---
+++ src/prog_menu/Menu_main.java
@@ -0,0 +1,13 @@
+package prog_menu;
+
+public class Menu_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Menu_view menu_view = new Menu_view();
+		menu_view.setBounds(5,5,755,455);
+		menu_view.setVisible(true);
+
+	}
+
+}
